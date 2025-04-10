d39c944bf5a123208f5caff3c021fe2f1e6002e5
FaisalFahad2 <Fhd20677@gmail.com>
2025-04-10 18:08:06 +0300

    UI of my answers

--- js/main.js
+++ js/main.js
@@ -23,3 +23,31 @@ document.addEventListener('click' , (e) => {
 
 
 
+
+function loadAnswers(questionId) {
+  const formData = new FormData();
+  formData.append("question_id", questionId);
+
+  fetch("../php/get-answers-comments.php", {
+    method: "POST",
+    body: formData
+  })
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById("answersContainer");
+    container.innerHTML = "";
+
+    data.answers.forEach(answer => {
+      const answerDiv = document.createElement("div");
+      answerDiv.className = "answer";
+      answerDiv.innerHTML = `
+        <p><strong>${answer.user}:</strong> ${answer.content}</p>
+        <button class="open-comment-modal" data-answer-id="${answer.id}">Add Comment</button>
+        <div class="comments">
+          ${answer.comments.map(comment => `<p><em>${comment.user}: ${comment.content}</em></p>`).join("")}
+        </div>
+      `;
+      container.appendChild(answerDiv);
+    });
+  });
+}
