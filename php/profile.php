aa5ba6cc05810820b87b36f6914df690325c6ccb
Your Name <you@example.com>
2025-05-03 15:14:21 +0300

    AHMAD

--- php/profile.php
+++ php/profile.php
@@ -32,7 +32,7 @@ $user = $stmt->get_result()->fetch_assoc();
   
     <div class="search-container">
       <input type="text" id="searchInput" placeholder="Search questions..." />
-      <button id="searchBtn" onclick="location.href='search-results.html'">
+      <button id="searchBtn" onclick="location.href='../html/search-results.html'">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
         </svg>
